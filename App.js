20480aeb83d1fabf7da4cd9e96500811bab303cd
Paulo Teixeira <paulo.teixeira541@gmail.com>
2020-10-06 11:48:49 -0300

    Terminei o app

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
-// import { Button } from 'react-native-paper';
+import { StyleSheet, View, Text } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
 
 class App extends React.Component {
 
@@ -8,7 +8,8 @@ class App extends React.Component {
     peso: 100,
     altura: 1.80,
     imc: 0,
-    legenda: 'Indeterminado'
+    legenda: 'Indeterminado',
+    cor: '#bdc3c7'
   };
 
   calcularIMC = () => {
@@ -20,23 +21,28 @@ class App extends React.Component {
 
     if (result < 18.5) {
       this.setState({
-        legenda: 'Abaixo do Peso'
+        legenda: 'Abaixo do Peso',
+        cor: '#e74c3c'
       });
     } else if (result >= 18.5 && result < 25) {
       this.setState({
-        legenda: 'Peso Normal'
+        legenda: 'Peso Normal',
+        cor: '#2ecc71'
       });
     } else if (result >= 25 && result < 30) {
       this.setState({
-        legenda: 'Sobrepeso'
+        legenda: 'Sobrepeso',
+        cor: '#f1c40f'
       });
     } else if (result >= 30 && result < 40) {
       this.setState({
-        legenda: 'Obesidade'
+        legenda: 'Obesidade',
+        cor: '#e67e22'
       });
     } else if (result >= 40) {
       this.setState({
-        legenda: 'Obesidade Grave'
+        legenda: 'Obesidade Grave',
+        cor: '#e74c3c'
       });
     }
   }
@@ -52,13 +58,16 @@ class App extends React.Component {
     return(
       <View style={styles.app}>
         <Text style={styles.legenda}>Seu IMC</Text>
-        <View>
+
+        <View style={[styles.painel, {backgroundColor: this.state.cor}]}>
           <Text style={styles.resultado}>{this.state.imc}</Text>
           <Text style={styles.diagnostico}>{this.state.legenda}</Text>
         </View>
+
         <View>
           <TextInput 
             style={styles.peso}
+            label="Peso"
             onChangeText={value => {
               this.setState({
                 peso: value.replace(',', '.')
@@ -67,13 +76,14 @@ class App extends React.Component {
           />
           <TextInput 
             style={styles.altura}
+            label="Altura"
             onChangeText={value => {
               this.setState({
                 altura: value.replace(',', '.')
               })
             }} 
           />
-          <Button title="Calcular" onPress={this.calcularIMC} />
+          <Button mode='contained' onPress={this.calcularIMC}>Calcular</Button>
         </View>
       </View>
     );
@@ -83,6 +93,13 @@ class App extends React.Component {
 const styles = StyleSheet.create({
   app: {
     padding: 10
+  },
+  painel: {
+    alignSelf: 'center',
+    borderRadius: 5,
+    width: 150,
+    marginVertical: 10,
+    padding: 8
   },
   legenda: {
     textAlign: 'center',
@@ -98,12 +115,10 @@ const styles = StyleSheet.create({
     fontSize: 16
   },
   peso: {
-    borderColor: '#000',
-    borderWidth: 1
+    marginVertical: 10
   },
   altura: {
-    borderColor: '#000',
-    borderWidth: 1
+    marginVertical: 10
   }
 });
 
